1aee8c3fe2fea023cc6100a0c7b0ac95079a057c
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-02-08 22:38:32 +0500

    Made navbar responsive by adding hamburger menu

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,14 +1,37 @@
+import { useState } from 'react';
 import logo from '../../assets/logo.jpg'
 import Navlinks from './NavLinks';
 
-
 const Navbar = () => {
 
+    const [menuToggle, setMenuToggle] = useState(false);
+
+  
     return (
         <div id='logo' className='w-full relative top-0 left-0 py-4 px-7'>
             <div className='w-full items-center flex justify-between px-6'>
                 <img src={logo} alt="" className='w-[100px] rounded-full h-[100px] object-[0px 100px] object-cover' />
-                <Navlinks />
+
+                <div className='max-sm:hidden block'>
+                    <Navlinks />
+                </div>
+
+                <div className='max-sm:block  hidden'>
+                    <label className="menuButton" htmlFor="check">
+                        <input type="checkbox"
+                            id="check"
+                            value={menuToggle}
+                            onClick={(e) => setMenuToggle(prev => !prev)}
+                        />
+                        <span className="top"></span>
+                        <span className="mid"></span>
+                        <span className="bot"></span>
+                    </label>
+                </div>
+
+                <div className={`max-sm:flex ${menuToggle ? 'left-0' : '-left-[1000px]'} bg-[#1C3144] transition-all duration-300 top-0 h-screen justify-center w-[40%]  items-center  absolute hidden`}>
+                    <Navlinks />
+                </div>
             </div>
         </div>
     );
